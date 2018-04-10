f2c2f4429dc21b3f6a1c13fc5b3fcf6e9d29ea06
lhj <1738761054@qq.com>
2018-04-10 13:27:30 +0800

    Update tran.sh

--- tran.sh
+++ tran.sh
@@ -2,5 +2,6 @@
 in_filename=$1
 out_filename=$2
 cat ${in_filename} | awk '{print $1 " " $4 " "4" "$6 " "$9 }' >> ${out_filename}
-
+sed -i 's/W/0/g' ${out_filename}
+sed -i 's/R/1/g' ${out_filename}
 
